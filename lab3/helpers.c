2594320c9675dc252089a060c5656a7051b42a0c
dario molina <darvak8@gmail.com>
2015-11-12 03:26:14 -0300

    haciendo broadcast de datos de temperatura..

--- lab3/helpers.c
+++ lab3/helpers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <mpi.h>
 
-static void input_data(int data_array[], int *rank, int *n, int *k, int *j){
+static void input_data(int data_array[], int* rank, int* n, int* k, int* j){
     if(rank==0){
         scanf("ingrese el tamaño de la matriz\n",n);
         scanf("ingrese la cantidad de iteraciones\n",k);
@@ -17,9 +17,10 @@ static void input_data_temp(int data_temp, int *rank, float *x, float *y, float 
         printf("ingrese los valores x y t");
         int i=0;
         while(scanf("%f %f %f",&x,&y,&t)!=EOF){
-            data_temp[i*3]=x;
-            data_temp[(i+1)*3]=y;
-            data_temp[(i+2)*3]=t;
+            data_temp[i]=x;
+            data_temp[i+1]=y;
+            data_temp[i+2]=t;
+            i+=1;
         }
     }
 }
--- lab3/temperatura.c
+++ lab3/temperatura.c
@@ -4,10 +4,31 @@
 #include "mpi.h"
 
 int main(int argc, char** argv) {
+
+    int comm_size, rank, n, k, j, data_array[];
+
     MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    //leemos los datos n, k, j por entrada standar y los guada en data_array
+    input_data(data_array, rank, n, k, j);
+    //leemos los datos x, y, t por entrada standar y los guarda en data_temp
+    input_data_temp(data_temp, rank, x, y, t);
 
 
+    //ajustamos el tamaño maximo de la matriz para que sea multiplo de comm_size <----------------------------------------ver!!!
+    //.... hacer ese paso, pensarlo!!!
 
+
+    //mandamos desde el proceso padre, a los procesos hijos
+    MPI_Bcast(data_array[0], 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(data_array[1], 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(data_array[2], 1, MPI_INT, 0, MPI_COMM_WORLD);
+    //enviamos los datos de la temperatura desde el ROOT a todos los procesos
+    MPI_Bcast(data_temp, j, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    
+    //CONTINUARAAAA..........!!!! fap fap 
 
     MPI_Finalize();
 return 0;
